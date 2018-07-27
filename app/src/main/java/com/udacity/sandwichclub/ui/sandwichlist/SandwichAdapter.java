ee89faf8cb10feb6a15faca9ac0f78ff3877b436
Yassin AJDI <yassin.ajdi@gmail.com>
2018-07-27 14:17:41 +0100

    Finish bind viewholder

--- app/src/main/java/com/udacity/sandwichclub/ui/sandwichlist/SandwichAdapter.java
+++ app/src/main/java/com/udacity/sandwichclub/ui/sandwichlist/SandwichAdapter.java
@@ -1,11 +1,15 @@
 package com.udacity.sandwichclub.ui.sandwichlist;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.databinding.ItemSandwichBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 
@@ -38,7 +42,30 @@ public class SandwichAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Sandwich sandwich = mSandwichList.get(position);
+        SandwichViewHolder sandwichViewHolder = (SandwichViewHolder) holder;
 
+        // sandwich image
+        Picasso.with(mContext)
+                .load(sandwich.getImage())
+                .placeholder(R.color.md_grey_200)
+                .into(sandwichViewHolder.binding.imageSandwich);
+
+        // sandwich name
+        sandwichViewHolder.binding.textName.setText(sandwich.getMainName());
+
+        // sandwich description
+        sandwichViewHolder.binding.textDescription.setText(sandwich.getDescription());
+
+        // click event
+        sandwichViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 27/07/2018 show sandwich details
+            }
+        });
+
+        sandwichViewHolder.binding.executePendingBindings();
     }
 
     @Override
