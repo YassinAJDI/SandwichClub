8f9ee998467b3d579749409e9bcfe5b77a6b1bd5
Yassin AJDI <yassin.ajdi@gmail.com>
2018-07-27 16:15:34 +0100

    Add sandwichlist viewmodel

--- app/src/main/java/com/udacity/sandwichclub/ui/sandwichlist/SandwichListViewModel.java
+++ app/src/main/java/com/udacity/sandwichclub/ui/sandwichlist/SandwichListViewModel.java
@@ -1,4 +1,33 @@
 package com.udacity.sandwichclub.ui.sandwichlist;
 
-public class SandwichListViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+import java.util.List;
+
+/**
+ * Exposes the data to be used in the sandwich list screen.
+ *
+ * @author Yassin Ajdi
+ */
+public class SandwichListViewModel extends AndroidViewModel {
+
+    private final Context mContext;
+
+    // MediatorLiveData can observe other LiveData objects and react on their emissions.
+//    private final MutableLiveData<List<Sandwich>> mObservableSandwiches;
+
+    public SandwichListViewModel(@NonNull Application application) {
+        super(application);
+
+        mContext = application.getApplicationContext();
+
+    }
+
+
 }
